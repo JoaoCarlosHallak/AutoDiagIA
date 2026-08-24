d72dc7f357f100fc63da9954202d61605f66f929
Mzg. <jchallaksantos@gmail.com>
2026-08-24 14:11:44 -0300

    UP

---
+++ src/main/java/com/autodiag/AutoDiagIA/AutoDiagIaApplication.java
@@ -0,0 +1,13 @@
+package com.autodiag.AutoDiagIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoDiagIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoDiagIaApplication.class, args);
+	}
+
+}
